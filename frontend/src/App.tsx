5717b4e626b83b7e325b45f9beb1df98894d1e17
aj <andrew.owens1010@gmail.com>
2023-06-05 17:59:38 -0400

    added player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,45 @@
 // App.tsx
-import React from 'react';
-import Navbar from './navbar'; // Make sure this path is correct
+import React, { useState } from 'react';
+import Navbar from './navbar';
 
 const App: React.FC = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  }
+
+  const handleSliderChange = (e) => {
+    setSliderValue(e.target.value);
+  }
+
   return (
     <div>
       <Navbar />
+      <div style={{
+        backgroundColor: 'white',
+        color: 'black',
+        border: '2px solid limegreen',
+        padding: '20px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        textAlign: 'center',
+        borderRadius: '4px',
+        height: '300px',
+        width: '450px', 
+      }}>
+        <p>Hello, this is your centered div with slime green border!</p>
+        
+        <div style={{position: 'absolute', bottom: '10px', width: '100%'}}>
+          <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
+          <input type="range" min="0" max="100" value={sliderValue} onChange={handleSliderChange} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
