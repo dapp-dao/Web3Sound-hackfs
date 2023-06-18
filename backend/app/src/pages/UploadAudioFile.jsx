78d0981f7555f02a775981238318ccd13cd6aad3
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2023-06-18 21:40:06 +0530

    Completed

--- backend/app/src/pages/UploadAudioFile.jsx
+++ backend/app/src/pages/UploadAudioFile.jsx
@@ -43,7 +43,7 @@ function UploadAudioFile() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false); // Track success message visibility
   const web3client = new Web3Storage({
     token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBBMjBDZGMwOWI5NjUwRDA3MWE3RWRjMjkwMjlkMDY5NDA2NENlNTQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODY2MTk4OTY0NjQsIm5hbWUiOiJBdWRpbyJ9.t3OwHAy0E6oL1LW79nI2foXeh26cLVYytGMhtN_0aJ4',
+      process.env.WEB3_TOKEN,
   });
   const history = useHistory();
 
